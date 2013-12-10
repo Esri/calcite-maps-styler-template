22e0eb24eda6ef33b36c275a72de367a0c850ac9
MikeTschudi <mtschudi@esri.com>
2013-12-10 08:38:16 -0800

    Merged branch release_3.0

--- Solutions/js2/lgonlineApp.js
+++ Solutions/js2/lgonlineApp.js
@@ -1,6 +1,6 @@
 /* 
 This source is a compressed form of part of the git commit 
-b897a317845d1885 2013-12-09 10:20:29 -0800
+36a521698e32e71e 2013-12-10 08:10:08 -0800
 Uncompressed source is available from https://github.com/Esri/local-government-online-apps 
 */ 
 /*
