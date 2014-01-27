622e430b6016d3ee7f03768c9a0f6f5a68bed981
Kelly Hutchins <khutchins@esri.com>
2014-01-27 08:37:41 -0800

    Update layout.js

--- Filter/javascript/layout.js
+++ Filter/javascript/layout.js
@@ -58,7 +58,7 @@ function createMap() {
        map = response.map;
         //If there's an application id and an application extent re-set the map extent to match 
         //the extent specified by the application item. 
-        if(options.appid && options.application_extent){
+        if(options.appid && options.application_extent.length > 0){
             var xmin, xmax, ymin, ymax;
             xmax = options.application_extent[1][0];
             xmin = options.application_extent[0][0];
