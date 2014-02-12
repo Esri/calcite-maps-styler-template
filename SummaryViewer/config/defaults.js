10cb0470fb12758eefab4ca3ccd7c147fe36635b
Kelly Hutchins <khutchins@esri.com>
2014-02-12 13:51:03 -0800

    Update defaults.js

--- SummaryViewer/config/defaults.js
+++ SummaryViewer/config/defaults.js
@@ -3,7 +3,7 @@ define([], function() {
     //default web map, default app color theme and more. These values can be overwritten by template configuration settings
     //and url parameters.
     var defaults = {
-        "appid": "4de27a9594804e8ca1b092f041def3b0",
+        "appid": "",
         "webmap": "55f15224d4da4b4e98504a4ff3a69119",
         "oauthappid": null, //"AFTKRmv16wj14N3z",
         //Group templates must support a group url parameter. This will contain the id of the group. 
@@ -43,7 +43,7 @@ define([], function() {
         },
         "cluster": true,
         "bingmapskey": "", //Enter the url to your organizations bing maps key if you want to use bing basemaps
-        "sharinghost": location.protocol + "//" + "devext.arcgis.com" //Defaults to arcgis.com. Set this value to your portal or organization host name. 
+        "sharinghost": location.protocol + "//" + "www.arcgis.com" //Defaults to arcgis.com. Set this value to your portal or organization host name. 
     };
     return defaults;
 });
