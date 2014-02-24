ac1e6e93351a2fec179a5c69162f5cad48c08bdc
MikeTschudi <mtschudi@esri.com>
2014-02-24 10:31:46 -0800

    Enhanced error display for unusable filter field #240

--- Solutions/js1/lgonlineStart.js
+++ Solutions/js1/lgonlineStart.js
@@ -1,6 +1,6 @@
 /* 
 This source is a compressed form of part of the git commit 
-bff75008d4d3bf6f 2014-02-21 14:09:15 -0800
+ba1aad0121b658b5 2014-02-24 10:26:59 -0800
 Uncompressed source is available from https://github.com/Esri/local-government-online-apps 
 */ 
 /*
