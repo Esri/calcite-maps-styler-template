61ed9ab017ad75e8eeb9a2b247858c00cb1f6329
MikeTschudi <mtschudi@esri.com>
2014-02-28 10:36:49 -0800

    qa release 3.2

--- Solutions/js2/lgonlineStart.js
+++ Solutions/js2/lgonlineStart.js
@@ -1,6 +1,10 @@
-/*global require,dojo,window,Modernizr,console,js,location,esri,defaultAppUI:true,alert */
-/*jslint sloppy:true */
-/** @license
+/* 
+This source is a compressed form of part of the git commit 
+45de21836277fa78 2014-02-28 10:29:48 -0800
+Uncompressed source is available from https://github.com/Esri/local-government-online-apps 
+*/ 
+/*
+
  | ArcGIS Solutions
  | Version 10.2
  | Copyright 2012 Esri
@@ -16,93 +20,6 @@
  | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  | See the License for the specific language governing permissions and
  | limitations under the License.
- */
-//============================================================================================================================//
-// Get the basic Dojo & Esri setup
-require(["dojo/ready", "dojo/Deferred", "esri/map", "dojo/i18n"], function (ready, Deferred) {
-    ready(function () {
-
-        // Define the String.trim() method if missing
-        // By Pradeep Kumar Mishra
-        // http://stackoverflow.com/a/498995
-        if (!String.prototype.trim) {
-            String.prototype.trim = function () {
-                return this.replace(/^\s+|\s+$/g, '');
-            };
-        }
-
-        // Supply a default app UI if one is not defined
-        if (!defaultAppUI) {
-            defaultAppUI = "apps2/GeneralMap";
-        }
-
-        // Bring in language files
-        dojo.requireLocalization("esriTemplate", "template");
-
-        // Load the UI loader
-        require(["dojo/ready", "js/lgonlineBuildUI"], function (ready) {
-            ready(function () {
-
-                // Load the UI elements
-                var uiElementsReady = new Deferred();
-                require(["dojo/ready", "js/lgonlineApp"], function (ready) {
-                    ready(function () {
-                        uiElementsReady.resolve();
-                    });
-                });
-
-                /**
-                 * Reports a launch error via an alert.
-                 * @param {number} messageNum The message to display; if this
-                 *        does not correspond to an existing message, message
-                 *        #0 (unable to launch app) is used
-                 * @param {boolean} [overwrite=false] Duplicate key-value pairs
-                 *        replace earlier ones (true) or values get appended to
-                 *        original key (false)
-                 */
-                function launchError(messageNum) {
-                    var i18n = dojo.i18n.getLocalization("esriTemplate", "template"),
-                        message;
-
-                    if (messageNum === 1) {
-                        message = i18n.messages.noConfiguration;
-                    } else {
-                        message = i18n.messages.unableToLaunchApp;
-                    }
-
-                    alert(message);
-                }
-
-                // Read the UI spec
-                (new js.LGUIBuilder(window.location.search, null, defaultAppUI)).ready.then(
-                    function (theBuilder) {
-                        uiElementsReady.then(function () {
-                            // Build the UI
-                            theBuilder.launch().then(
-                                function () {
-                                    // Reveal the content and hide the loading indicator
-                                    dojo.fadeIn({
-                                        node: "contentDiv",
-                                        duration: 500,
-                                        onEnd: function () {
-                                            dojo.removeClass("contentDiv", "transparent");
-                                            dojo.removeClass("pageBody", "startupBkgd");
-                                        }
-                                    }).play();
-                                    console.log("Application is ready");//???
-                                },
-                                function () {
-                                    launchError(0);
-                                }
-                            );
-                        });
-                    },
-                    function () {
-                        launchError(1);
-                    }
-                );
-
-            });
-        });
-    });
-});
+*/
+require(["dojo/ready","dojo/Deferred","esri/map","dojo/i18n"],function(a,d){a(function(){String.prototype.trim||(String.prototype.trim=function(){return this.replace(/^\s+|\s+$/g,"")});defaultAppUI||(defaultAppUI="apps2/GeneralMap");dojo.requireLocalization("esriTemplate","template");require(["dojo/ready","js/lgonlineBuildUI"],function(a){a(function(){function a(c){var b=dojo.i18n.getLocalization("esriTemplate","template");alert(1===c?b.messages.noConfiguration:b.messages.unableToLaunchApp)}var b=
+new d;require(["dojo/ready","js/lgonlineApp"],function(a){a(function(){b.resolve()})});(new js.LGUIBuilder(window.location.search,null,defaultAppUI)).ready.then(function(c){b.then(function(){c.launch().then(function(){dojo.fadeIn({node:"contentDiv",duration:500,onEnd:function(){dojo.removeClass("contentDiv","transparent");dojo.removeClass("pageBody","startupBkgd")}}).play();console.log("Application is ready")},function(){a(0)})})},function(){a(1)})})})})});
