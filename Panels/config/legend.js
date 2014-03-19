ebafbecc939dbbdaa743480bc44f4480275cef53
Kelly Hutchins <khutchins@esri.com>
2014-03-19 13:26:24 -0700

    Update legend.js

--- Panels/config/legend.js
+++ Panels/config/legend.js
@@ -39,6 +39,7 @@ define([], function() {
         //LEGEND: Display the legend in a panel on the right or left side of the map.
         "legend":  true,
         "legend_side": "right",//left or right 
+        "localize": true,
         /****************Optional Widgets*******************/
         "home_button": true,
         "basemap_option": "", //valid values are street, satellite, hybrid, topo, gray, oceans, national-geographic and osm
