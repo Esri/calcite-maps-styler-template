b4878e3797a84f992409bd46b96b1c89b16b82a2
Kelly Hutchins <khutchins@esri.com>
2014-05-09 14:53:37 -0700

    Update layout.js
    Update for #348

--- bv_build/src/js/layout.js
+++ bv_build/src/js/layout.js
@@ -1490,7 +1490,7 @@ function createOptions() {
             hasEsri = true;
             geocoder.name = "Esri World Geocoder";
             geocoder.outFields = "Match_addr, stAddr, City";
-            geocoder.singleLineFieldName = "Single Line";
+            geocoder.singleLineFieldName = "SingleLine";
             geocoder.placeholder = (configOptions.placefinder.placeholder === "") ? i18n.tools.search.title : configOptions.placefinder.placeholder;
             geocoder.esri = geocoder.placefinding = true;
             if (configOptions.placefinder.currentExtent || configOptions.searchextent) {
