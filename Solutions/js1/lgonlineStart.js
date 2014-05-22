e337435d8fbfdd33dab9671331fc611a92aef85b
MikeTschudi <mtschudi@esri.com>
2014-05-22 16:49:03 -0700

    Changed basemap gallery to use webmap's thumbnail for the webmap if an alternate thumbnail has not been configured #271

--- Solutions/js1/lgonlineStart.js
+++ Solutions/js1/lgonlineStart.js
@@ -1,6 +1,6 @@
 /* 
 This source is a compressed form of part of the git commit 
-dd65ace97c3b4212 2014-05-22 16:11:25 -0700
+4150f1ff6f995e72 2014-05-22 16:47:06 -0700
 Uncompressed source is available from https://github.com/Esri/local-government-online-apps 
 */ 
 /*
