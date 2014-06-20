c92ec4f3c361e8db188c031ca955f8ec7262c187
Jonathan Turpin <jturpin@esri.com>
2014-06-20 12:20:49 -0700

    3.4 Pseudo HO05 Translations (WebGIS/arcgis-portal-app#1418)

---
+++ SimpleViewer/js/nls/nb/resources.js
@@ -0,0 +1,12 @@
+﻿define(
+   ({
+        map: {
+            error: "å_Unable to create map_ø"
+        }, 
+        tools: {
+        	legend: "å_Legend_ø",
+        	 about: "å_About_ø"
+        }
+    })
+   
+);
