fe519ba00a76c81c09fe36d64cce302b57f24a41
Kelly Hutchins <khutchins@esri.com>
2014-08-12 15:31:41 -0700

    Update main.js
    Update for #462

--- Viewer/js/main.js
+++ Viewer/js/main.js
@@ -181,8 +181,15 @@ ready, JSON, array, Color, declare, lang, dom, domGeometry, domAttr, domClass, d
                     //Now that all the tools have been added to the toolbar we can add page naviagation
                     //to the toolbar panel, update the color theme and set the active tool. 
                     this._updateTheme();
-                    toolbar.activateTool(this.config.activeTool);
-                    toolbar.updatePageNavigation();
+                    
+                    
+                   if(this.config.activeTool){
+                        toolbar.activateTool(this.config.activeTool);
+                        toolbar.updatePageNavigation();
+                    }else{
+                        toolbar._closePage();
+                    }
+          
 
                     on(toolbar, "updateTool", lang.hitch(this, function (name) {
                         if (name === "measure") {
@@ -414,7 +421,9 @@ ready, JSON, array, Color, declare, lang, dom, domGeometry, domAttr, domClass, d
                     }, domConstruct.create("div", {}, legendDiv));
                     domClass.add(legend.domNode, "legend");
                     legend.startup();
-                    toolbar.activateTool(this.config.activeTool || "legend");
+                    if(this.config.activeTool){
+                     toolbar.activateTool(this.config.activeTool || "legend");
+                    }
                     deferred.resolve(true);
 
                 } else {
