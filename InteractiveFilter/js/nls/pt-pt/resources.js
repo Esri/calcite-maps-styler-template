37805f66edfb26d107b720e539acb78342829e14
Kelly Hutchins <khutchins@esri.com>
2014-08-18 08:52:19 -0700

    Fix NLS case issue

---
+++ InteractiveFilter/js/nls/pt-pt/resources.js
@@ -0,0 +1,18 @@
+﻿define(
+   ({
+  	map: {
+  		error: "Não foi possível criar o mapa"
+  	},
+    viewer: {
+      content_title: "Filtro",
+      button_text: "Aplicar",
+      filterInstructions: "Filtre a camada definindo valores.",
+      filterOr: "Qualquer uma das expressões seguintes tem de ser verdadeira.",
+      filterAnd: "Todas as expressões seguintes têm de ser verdadeiras.",
+      filterNo:  "Web map does not contain any interactive filters.",
+      errors:{
+        message: "Problema ao criar a aplicação de filtros"
+      }
+    }
+  })
+);
---
+++ InteractiveFilter/js/nls/zh-cn/resources.js
@@ -0,0 +1,18 @@
+﻿define(
+   ({
+  	map: {
+  		error: "无法创建地图"
+  	},
+    viewer: {
+      content_title: "过滤器",
+      button_text: "应用",
+      filterInstructions: "通过指定值来过滤图层。",
+      filterOr: "以下任意表达式必须为 true。",
+      filterAnd: "以下表达式必须全部为 true。",
+      filterNo:  "Web map does not contain any interactive filters.",
+      errors:{
+        message: "创建过滤器应用时出现问题"
+      }
+    }
+  })
+);
