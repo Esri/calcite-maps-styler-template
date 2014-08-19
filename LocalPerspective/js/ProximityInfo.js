9be31ef8bcde4a7aed5e0c2ea895b5a916098523
Sajit Thomas <sthomas@esri.com>
2014-08-19 10:25:05 -0400

    Updated Proximity Info.

--- LocalPerspective/js/ProximityInfo.js
+++ LocalPerspective/js/ProximityInfo.js
@@ -146,22 +146,18 @@ define([
                var num = i+1;
                
                var rec = domConstruct.create("div", {
-                  //class: "recProximity"
                }, content);
                domClass.add(rec, 'recProximity');
                var recLeftNum = domConstruct.create("div", {
-                  //class: "recLeftNum"
                }, rec);
                 domClass.add(recLeftNum, 'recLeftNum');
                var recNum = domConstruct.create("div", {
-                  //class: "recNum",
                   style: "background-color:" + this.pageObj.color,
                   innerHTML: num
                }, recLeftNum);
                domClass.add(recNum, 'recNum');
                on(recNum, "click", lang.hitch(this, this.zoomToLocation, gra, pt));
                var recInfo = domConstruct.create("div", {
-                  //class: "recInfo"
                }, rec);
                 domClass.add(recInfo, 'recInfo');
                var infoDist = "";
@@ -169,7 +165,6 @@ define([
                   infoDist += "<img src='images/car.png' /> ";
                infoDist  += Math.round(dist*100)/100 + " " + this.config.distanceUnits.toUpperCase() + "<br/>";
                var recDistance = domConstruct.create("span", {
-                  //class: "recDistance",
                   innerHTML: infoDist
                }, recInfo);
                domClass.add(recDistance, 'recDistance');
