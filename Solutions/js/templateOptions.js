81cd38031f474150fb246a3d195dbd3eee380712
mike6491 <mtschudi@esri.com>
2014-09-02 14:33:08 -0700

    Solutions 3.5 release candidate

--- Solutions/js/templateOptions.js
+++ Solutions/js/templateOptions.js
@@ -49,4 +49,3 @@ define({
         "obj" // object id
     ]
 });
-
--- Solutions/js1/lgonlineStart.js
+++ Solutions/js1/lgonlineStart.js
@@ -1,6 +1,10 @@
-﻿/*global require,dojo,Modernizr,console,js,esri */
-/*jslint browser:true,sloppy:true,nomen:true,unparam:true,plusplus:true */
-/** @license
+/* 
+This source is a compressed form of part of the git commit 
+83d92ccbe5aa7412 2014-09-02 14:21:36 -0700
+Uncompressed source is available from https://github.com/Esri/local-government-online-apps 
+*/ 
+/*
+
  | Copyright 2012 Esri
  |
  | Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,79 +18,6 @@
  | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  | See the License for the specific language governing permissions and
  | limitations under the License.
- */
-//============================================================================================================================//
-// Get the basic Dojo & Esri setup
-require(["dojo/ready", "dojo/_base/Deferred", "esri/map", "dojo/i18n"], function (ready) {
-    ready(function () {
-
-        // Define the String.trim() method if missing
-        // By Pradeep Kumar Mishra
-        // http://stackoverflow.com/a/498995
-        if (!String.prototype.trim) {
-            String.prototype.trim = function () {
-                return this.replace(/^\s+|\s+$/g, '');
-            };
-        }
-
-        // Bring in language files
-        dojo.requireLocalization("esriTemplate", "resources");
-
-        // Normalize the browser features
-        Modernizr.load([{
-            test: window.JSON,
-            nope: 'js/json2.js',
-            complete: function () {
-
-                // Load the UI loader
-                require(["dojo/ready", "js/lgonlineBuildUI"], function (ready) {
-                    ready(function () {
-
-                        // Load the UI elements
-                        var uiElementsReady = new dojo.Deferred();
-                        require(["dojo/ready", "js/lgonlineApp"], function (ready) {
-                            ready(function () {
-                                uiElementsReady.resolve();
-                            });
-                        });
-
-                        // Read the UI spec
-                        (new js.LGUIBuilder(window.location.search, null, "apps1/ParcelViewer")).ready.then(
-                            function (theBuilder) {
-                                uiElementsReady.then(function () {
-                                    // Build the UI
-                                    theBuilder.launch().then(
-                                        function () {
-                                            // Reveal the content and hide the loading indicator
-                                            dojo.fadeIn({
-                                                node: "contentDiv",
-                                                duration: 500,
-                                                onEnd: function () {
-                                                    dojo.removeClass("contentDiv", "transparent");
-                                                    dojo.removeClass("pageBody", "startupBkgd");
-                                                }
-                                            }).play();
-                                            console.log("Application is ready");//???
-                                        },
-                                        function () {
-                                            console.error("Unable to launch application");//???
-                                        }
-                                    );
-                                });
-                            },
-                            function () {
-                                console.error("Unable to find configuration");//???
-                            }
-                        );
-
-                    });
-                });
-            }
-        }]);
-    });
-});
-/* 
-This source is part of the git commit 
-5e8776036f46a8a2 2014-09-02 07:17:15 -0700
-It is available from https://github.com/Esri/local-government-online-apps 
-*/ 
+*/
+require(["dojo/ready","dojo/_base/Deferred","esri/map","dojo/i18n"],function(a){a(function(){String.prototype.trim||(String.prototype.trim=function(){return this.replace(/^\s+|\s+$/g,"")});dojo.requireLocalization("esriTemplate","resources");Modernizr.load([{test:window.JSON,nope:"js/json2.js",complete:function(){require(["dojo/ready","js/lgonlineBuildUI"],function(a){a(function(){var a=new dojo.Deferred;require(["dojo/ready","js/lgonlineApp"],function(b){b(function(){a.resolve()})});(new js.LGUIBuilder(window.location.search,
+null,"apps1/ParcelViewer")).ready.then(function(b){a.then(function(){b.launch().then(function(){dojo.fadeIn({node:"contentDiv",duration:500,onEnd:function(){dojo.removeClass("contentDiv","transparent");dojo.removeClass("pageBody","startupBkgd")}}).play();console.log("Application is ready")},function(){console.error("Unable to launch application")})})},function(){console.error("Unable to find configuration")})})})}}])})});
