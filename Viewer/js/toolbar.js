02aafead84518cb43b0e6513bd1c7f2effb0df87
Kelly Hutchins <khutchins@esri.com>
2014-09-18 15:50:16 -0700

    Scroll wheel updates for #425

--- Viewer/js/toolbar.js
+++ Viewer/js/toolbar.js
@@ -34,17 +34,35 @@ Evented, declare, win, fx, html, lang, has, dom, domClass, domStyle, domAttr, do
             var deferred;
 
             deferred = new Deferred();
-
+            on(window, "scroll", lang.hitch(this, this._windowScrolled));
+            on(window, "resize", lang.hitch(this, this._windowScrolled));
             this.pTools = dom.byId("panelTools");
             this.pMenu = dom.byId("panelMenu");
             on(this.pMenu, "click", lang.hitch(this, this._menuClick));
             this.pPages = dom.byId("panelPages");
             //Prevent body scroll when scrolling to the end of the panel content
+
             on(this.pPages, mouse.enter, lang.hitch(this, function () {
+
+                if (this._hasScrollbar()) {
+                    var p = dom.byId("panelContent");
+                    if (p) {
+                        domClass.add(p, "modal-scrollbar");
+                    }
+                }
                 domStyle.set(win.body(), "overflow", "hidden");
+
             }));
             on(this.pPages, mouse.leave, lang.hitch(this, function () {
-                domStyle.set(win.body(), "overflow", "");
+                if (this._hasScrollbar === false) {
+                    var p = dom.byId("panelContent");
+                    if (p) {
+                        domClass.remove(p, "modal-scrollbar");
+                    }
+                    domStyle.set(win.body(), "overflow-y", "auto");
+                }
+
+
             }));
             domConstruct.empty(this.pPages);
             // add blank page
@@ -57,7 +75,33 @@ Evented, declare, win, fx, html, lang, has, dom, domClass, domStyle, domAttr, do
 
             return deferred.promise;
         },
+        _hasScrollbar: function () {
+            // The Modern solution
+            if (typeof window.innerWidth === 'number') return window.innerWidth > document.documentElement.clientWidth;
 
+            // rootElem for quirksmode
+            var rootElem = document.documentElement || document.body;
+
+            // Check overflow style property on body for fauxscrollbars
+            var overflowStyle;
+
+            if (typeof rootElem.currentStyle !== 'undefined') overflowStyle = rootElem.currentStyle.overflow;
+
+            overflowStyle = overflowStyle || window.getComputedStyle(rootElem, '').overflow;
+
+            // Also need to check the Y axis overflow
+            var overflowYStyle;
+
+            if (typeof rootElem.currentStyle !== 'undefined') overflowYStyle = rootElem.currentStyle.overflowY;
+
+            overflowYStyle = overflowYStyle || window.getComputedStyle(rootElem, '').overflowY;
+
+            var contentOverflows = rootElem.scrollHeight > rootElem.clientHeight;
+            var overflowShown = /^(visible|auto)$/.test(overflowStyle) || /^(visible|auto)$/.test(overflowYStyle);
+            var alwaysShowScroll = overflowStyle === 'scroll' || overflowYStyle === 'scroll';
+
+            return (contentOverflows && overflowShown) || (alwaysShowScroll);
+        },
         //Create a tool and return the div where you can place content
         createTool: function (tool, panelClass) {
             var name = tool.name;
@@ -227,7 +271,16 @@ Evented, declare, win, fx, html, lang, has, dom, domClass, domStyle, domAttr, do
 
         },
 
+        // window scrolled
+        _windowScrolled: function (evt) {
 
+            if (this.scrollTimer) {
+                clearTimeout(this.scrollTimer);
+            }
+            if (this.snap === true) {
+                this.scrollTimer = setTimeout(lang.hitch(this, this._snapScroll), 300);
+            }
+        },
         _snapScroll: function () {
 
             var startPos = domGeometry.docScroll().y;
