0f41b114f6aa8f2009f01c09be4f00b631d0826a
Allison Muise <amuise@esri.com>
2014-09-24 15:32:42 -0700

    Update to fix issue with old config files not working.

--- Solutions/js1/lgonlineBuildUI.js
+++ Solutions/js1/lgonlineBuildUI.js
@@ -1,6 +1,6 @@
 /* 
 This source is a compressed form of part of the git commit 
-fea82736b7cbb5bb 2014-09-05 15:35:57 -0700
+bc139eb0a70f57cc 2014-09-24 15:23:32 -0700
 Uncompressed source is available from https://github.com/Esri/local-government-online-apps 
 */ 
 /*
