597483ca09c329b2f1b447d70158ca68e73079fb
Kelly Hutchins <khutchins@esri.com>
2014-10-22 17:07:06 -0700

    Update configurationPanel.js
    add config options for compare template

--- Compare/resources/configurationPanel.js
+++ Compare/resources/configurationPanel.js
@@ -1,30 +1,50 @@
 {
-    "configurationSettings": [{
-        "category": "<b>Configure template</b>",
-        "fields": [{
-            "type": "webmap"
-        },{
-            "placeHolder": "Defaults to map owner",
-            "label": "Owner Text:",
-            "fieldName": "owner",
-            "type": "string",
-            "tooltip": "Defaults to map owner"
-        }, {
-            "type": "string",
-            "fieldName": "theme",
-            "tooltip": "Color theme to use",
-            "label": "Color Scheme:",
-            "options": [{
-                "label": "Chrome",
-                "value": "chrome"
-            }, {
-                "label": "Seaside",
-                "value": "seaside"
-            }, {
-                "label": "Pavement",
-                "value": "pavement"
-            }]
-        }]
-    }],
-    "values": {}
+    "configurationSettings": [
+        {
+            "category": "<b>Configure template</b>",
+            "fields": [
+                {
+                    "type": "webmap",
+                    "selection": "multiple"
+                },
+                {
+                    "placeHolder": "Defaults to Compare Maps",
+                    "type": "string",
+                    "fieldName": "title",
+                    "label": "Title",
+                    "tooltip": "Defaults to Compare Maps"
+                },
+                {
+                    "type": "boolean",
+                    "fieldName": "home",
+                    "label": "Home Extent Button"
+                },
+                {
+                    "type": "string",
+                    "fieldName": "description",
+                    "label": "Details",
+                    "placeHolder": "Provide a description for the application",
+                    "tooltip": "Enter a description for the application",
+                    "stringFieldOption": "richtext"
+                },
+                {
+                    "type": "color",
+                    "fieldName": "theme_color",
+                    "tooltip": "Text color",
+                    "label": "Text color:"
+                },
+                {
+                    "type": "color",
+                    "fieldName": "theme_bg_color",
+                    "tooltip": "Background color for panel",
+                    "label": "Panel background color:"
+                }
+            ]
+        }
+    ],
+    "values": {
+        "home": true,
+        "theme_color": "#575757",
+        "theme_bg_color": "#fff"
+    }
 }
