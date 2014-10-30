85b2db35edbff7a2bd75a7823f118251846837cd
Sajit Thomas <sthomas@esri.com>
2014-10-30 10:11:06 -0400

    Update NLS support for VI

--- LocalPerspective/js/nls/resources.js
+++ LocalPerspective/js/nls/resources.js
@@ -15,52 +15,53 @@
  | limitations under the License.
  */
 define({
-    root: ({
-        "map" : {
-            "error" : "Unable to create map"
-        },
-        "days" : {
-           "now" : "NOW",
-           "sun" : "SUN",
-           "mon" : "MON",
-           "tue" : "TUE",
-           "wed" : "WED",
-           "thu" : "THU",
-           "fri" : "FRI",
-           "sat" : "SAT"
-        },
-        "tooltips" : {
-           "logo" : "Logo",
-           "menu" : "Menu",
-           "close" : "Close",
-           "previous" : "Previous",
-           "next" : "Next",
-           "directions" : "Directions"
-        }
-    }),
-  "ar":1,
-  "cs":1,
-  "da":1,
-  "de":1,
-  "es":1,
-  "et":1,
-  "fi":1,
-  "fr":1,
-  "he":1,
-  "it":1,
-  "ja":1,
-  "ko":1,
-  "lt":1,
-  "lv":1,
-  "nl":1,
-  "nb":1,
-  "pl":1,
-  "pt-br":1,
-  "pt-pt":1,
-  "ro":1,
-  "ru":1,
-  "sv":1,
-  "tr":1,
-  "th":1,
-  "zh-cn":1
+   root : ( {
+      "map" : {
+         "error" : "Unable to create map"
+      },
+      "days" : {
+         "now" : "NOW",
+         "sun" : "SUN",
+         "mon" : "MON",
+         "tue" : "TUE",
+         "wed" : "WED",
+         "thu" : "THU",
+         "fri" : "FRI",
+         "sat" : "SAT"
+      },
+      "tooltips" : {
+         "logo" : "Logo",
+         "menu" : "Menu",
+         "close" : "Close",
+         "previous" : "Previous",
+         "next" : "Next",
+         "directions" : "Directions"
+      }
+   }),
+   "ar" : 1,
+   "cs" : 1,
+   "da" : 1,
+   "de" : 1,
+   "es" : 1,
+   "et" : 1,
+   "fi" : 1,
+   "fr" : 1,
+   "he" : 1,
+   "it" : 1,
+   "ja" : 1,
+   "ko" : 1,
+   "lt" : 1,
+   "lv" : 1,
+   "nl" : 1,
+   "nb" : 1,
+   "pl" : 1,
+   "pt-br" : 1,
+   "pt-pt" : 1,
+   "ro" : 1,
+   "ru" : 1,
+   "sv" : 1,
+   "tr" : 1,
+   "th" : 1,
+   "vi" : 1,
+   "zh-cn" : 1
 });
