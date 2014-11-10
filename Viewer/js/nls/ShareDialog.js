2025d85ca7deed448a85d9de5da0578e4838efeb
Kelly Hutchins <khutchins@esri.com>
2014-11-10 13:55:13 -0800

    Update ShareDialog.js
    update nls format for #558

--- Viewer/js/nls/ShareDialog.js
+++ Viewer/js/nls/ShareDialog.js
@@ -1,17 +1,17 @@
 define({
-    "root": ({
-        "widgets": {
-            "ShareDialog": {
-                "title": "Share",
-                "heading": "Share this map",
-                "url": "Map Link",
-                "embed": "Embed Map",
-                "extent": "Share current map extent",
-                "size": "Size (width/height):",
-                "facebookTooltip": "Facebook",
-                "twitterTooltip": "Twitter",
-                "gplusTooltip": "Google Plus",
-                "emailTooltip": "Email"
+    root: ({
+        widgets: {
+            ShareDialog: {
+                title: "Share",
+                heading: "Share this map",
+                url: "Map Link",
+                embed: "Embed Map",
+                extent: "Share current map extent",
+                size: "Size (width/height):",
+                facebookTooltip: "Facebook",
+                twitterTooltip: "Twitter",
+                gplusTooltip: "Google Plus",
+                emailTooltip: "Email"
             }
         }
     }),
