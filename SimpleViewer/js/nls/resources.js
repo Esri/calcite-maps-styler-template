ba4d410d92147dd9f2f07949e40f79dd7cb76e92
Kelly Hutchins <khutchins@esri.com>
2014-11-10 16:44:53 -0800

    update to support search extent for #459

--- SimpleViewer/js/nls/resources.js
+++ SimpleViewer/js/nls/resources.js
@@ -3,7 +3,14 @@ define({
         map: {
             error: "Unable to create map"
         }, 
-        tools: {
+        tools: {         
+            search: {
+                error: "Location could not be found",
+                notWhatYouWanted: "Not what you wanted?",
+                selectAnother: "Select another location",
+                currentLocation: "Current Location",
+                title: "Location"
+            },
         	legend: "Legend",
         	 about: "About"
         }
