ab8d700b4d6eab4ffeeadf1d01876d8a57dde5e6
Ian Wittenmyer <iwittenmyer@esri.com>
2014-12-08 14:14:04 -0800

    Update to 3.12 on production

--- ImpactSummary/js/TemplateBuilder.js
+++ ImpactSummary/js/TemplateBuilder.js
@@ -100,7 +100,7 @@ function (
         _loadCSS: function () {
             //Load claro css
             if (dom.byId("claroTheme")) {
-                domAttr.set(dom.byId("claroTheme"), "href", location.protocol + "//jsdev.arcgis.com/3.12/dijit/themes/claro/claro.css");
+                domAttr.set(dom.byId("claroTheme"), "href", location.protocol + "//js.arcgis.com/3.12/dijit/themes/claro/claro.css");
             }
             domClass.add(document.body, "claro");
             //Load browser dialog
