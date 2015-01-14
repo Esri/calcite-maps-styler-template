a172e6e32c57fb1f2d5a1ab5045710a530174db0
Kelly Hutchins <khutchins@esri.com>
2015-01-13 17:30:18 -0800

    Update toolbar.js
    fix indentation

--- Viewer/js/toolbar.js
+++ Viewer/js/toolbar.js
@@ -116,7 +116,7 @@ Evented, declare, win, fx, html, lang, has, dom, domClass, domStyle, domAttr, do
                 var tip = this.config.i18n.tooltips[name] || name;
                 domAttr.set(pTool, "data-title", tip);
                 domAttr.set(pTool, "title", tip);
-                    on(pTool, mouse.enter, function(){
+                on(pTool, mouse.enter, function(){
                     domAttr.set(pTool, "title","");
                 });
                 on(pTool, mouse.leave, function(){
