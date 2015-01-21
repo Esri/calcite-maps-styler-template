e002a451fa67f4093a86dfe3d83bca1f290389ac
mike6491 <mtschudi@esri.com>
2015-01-21 05:29:03 -0800

    Added language switches to Constituent*
    #644

--- ConstituentEngagementConsole/js/nls/en/resources.js
+++ ConstituentEngagementConsole/js/nls/en/resources.js
@@ -15,8 +15,8 @@
  | limitations under the License.
  */
 //Lbl-Label, Txt-Text, Err-Error, Msg-Message
-define({
-    root: ({
+define(
+    ({
         map: {
             error: "Unable to create map"
         },
@@ -64,33 +64,5 @@ define({
         config: {
             configNotDefined: "Config is not defined"
         }
-    }),
-    "ar": 1,
-    "cs": 1,
-    "da": 1,
-    "de": 1,
-    "el": 1,
-    "en": 1,
-    "es": 1,
-    "et": 1,
-    "fi": 1,
-    "fr": 1,
-    "he": 1,
-    "it": 1,
-    "ja": 1,
-    "ko": 1,
-    "lt": 1,
-    "lv": 1,
-    "nb": 1,
-    "nl": 1,
-    "pl": 1,
-    "pt-br": 1,
-    "pt-pt": 1,
-    "ro": 1,
-    "ru": 1,
-    "sv": 1,
-    "th": 1,
-    "tr": 1,
-    "vi": 1,
-    "zh-cn": 1
-});
+    })
+);
