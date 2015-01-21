7c3906ae43c2aad3ec3e8cc2db677aaeee6d10a1
Kelly Hutchins <khutchins@esri.com>
2015-01-21 13:10:00 -0800

    Update config panel for new edit template

--- Editor/resources/configurationPanel.js
+++ Editor/resources/configurationPanel.js
@@ -1,42 +1,101 @@
 {  
-  "configurationSettings":[  
-    {  
-      "category":"<b>Configure template</b>",
-      "fields":[  
-        {  
-          "type":"webmap"
-        },
-        {  
-          "placeHolder":"Defaults to map owner",
-          "label":"Owner Text:",
-          "fieldName":"owner",
-          "type":"string",
-          "tooltip":"Defaults to map owner"
-        },
-        {  
-          "type":"string",
-          "fieldName":"theme",
-          "tooltip":"Color theme to use",
-          "label":"Color Scheme:",
-          "options":[  
+   "configurationSettings":[  
+      {  
+         "category":"<b>Configure template</b>",
+         "fields":[  
             {  
-              "label":"Chrome",
-              "value":"chrome"
+               "type":"webmap"
             },
             {  
-              "label":"Seaside",
-              "value":"seaside"
+               "placeHolder":"Defaults to web map title",
+               "label":"Title:",
+               "fieldName":"title",
+               "type":"string",
+               "tooltip":"Defaults to web map title"
             },
             {  
-              "label":"Pavement",
-              "value":"pavement"
+               "type":"color",
+               "fieldName":"theme",
+               "tooltip":"Color theme to use",
+               "label":"Color Theme:"
+            },
+            {  
+               "type":"color",
+               "fieldName":"color",
+               "tooltip":"Title bar text color",
+               "label":"Title Color:"
+            },
+            {  
+               "type":"boolean",
+               "fieldName":"edittoolbar",
+               "label":"Display Edit Toolbar"
             }
-          ]
-        }
-      ]
-    }
-  ],
-  "values":{  
-
-  }
+         ]
+      },
+      {  
+         "category":"Tools",
+         "fields":[  
+            {  
+               "type":"paragraph",
+               "value":"Select search layers and fields. These layers will appear in the search tool allowing application users to search for particular values in the specified layers and fields."
+            },
+            {  
+               "label":"Select layers and fields",
+               "fieldName":"searchLayers",
+               "type":"multilayerandfieldselector",
+               "tooltip":"Select layer and fields to search",
+               "layerOptions":{  
+                  "supportedTypes":[  
+                     "FeatureLayer"
+                  ],
+                  "geometryTypes":[  
+                     "esriGeometryPoint",
+                     "esriGeometryLine",
+                     "esriGeometryPolyline",
+                     "esriGeometryPolygon"
+                  ]
+               },
+               "fieldOptions":{  
+                  "supportedTypes":[  
+                     "esriFieldTypeString"
+                  ]
+               }
+            },
+            {  
+               "type":"boolean",
+               "fieldName":"search",
+               "label":"Address Finder"
+            },
+            {  
+               "type":"boolean",
+               "fieldName":"searchExtent",
+               "label":"Prioritize search results in current extent."
+            },
+            {  
+               "type":"boolean",
+               "fieldName":"locate",
+               "label":"Locate Button"
+            },
+            {  
+               "type":"boolean",
+               "fieldName":"home",
+               "label":"Home Button"
+            },
+            {  
+               "type":"boolean",
+               "fieldName":"scale",
+               "label":"Scalebar"
+            }
+         ]
+      }
+   ],
+   "values":{  
+      "color":"#4c4c4c",
+      "theme":"#f7f8f8",
+      "scale":true,
+      "home":true,
+      "locate":false,
+      "search":true,
+      "searchExtent":false
+   }
 }
