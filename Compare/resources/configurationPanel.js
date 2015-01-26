d71b159aab9cd7910444a53911811772c2cffec3
Kelly Hutchins <khutchins@esri.com>
2015-01-26 13:41:00 -0800

    Update configurationPanel.js
    reorganize config contents

--- Compare/resources/configurationPanel.js
+++ Compare/resources/configurationPanel.js
@@ -1,50 +1,60 @@
 {
-    "configurationSettings": [
-        {
-            "category": "<b>Configure template</b>",
-            "fields": [
-                {
-                    "type": "webmap",
-                    "selection": "multiple"
-                },
-                {
-                    "placeHolder": "Defaults to Compare Maps",
-                    "type": "string",
-                    "fieldName": "title",
-                    "label": "Title",
-                    "tooltip": "Defaults to Compare Maps"
-                },
-                {
-                    "type": "boolean",
-                    "fieldName": "home",
-                    "label": "Home Extent Button"
-                },
-                {
-                    "type": "string",
-                    "fieldName": "description",
-                    "label": "Details",
-                    "placeHolder": "Provide a description for the application",
-                    "tooltip": "Enter a description for the application",
-                    "stringFieldOption": "richtext"
-                },
-                {
-                    "type": "color",
-                    "fieldName": "theme_color",
-                    "tooltip": "Text color",
-                    "label": "Text color:"
-                },
-                {
-                    "type": "color",
-                    "fieldName": "theme_bg_color",
-                    "tooltip": "Background color for panel",
-                    "label": "Panel background color:"
-                }
-            ]
-        }
-    ],
-    "values": {
-        "home": true,
-        "theme_color": "#575757",
-        "theme_bg_color": "#fff"
-    }
+   "configurationSettings":[
+      {
+         "category":"<b>Map Properties</b>",
+         "fields":[
+            {
+               "type":"webmap",
+               "selection":"multiple"
+            },
+            {
+               "placeHolder":"Defaults to Compare Maps",
+               "type":"string",
+               "fieldName":"title",
+               "label":"Title",
+               "tooltip":"Defaults to Compare Maps"
+            },
+            {
+               "type":"string",
+               "fieldName":"description",
+               "label":"Details",
+               "placeHolder":"Provide a description for the application",
+               "tooltip":"Enter a description for the application",
+               "stringFieldOption":"richtext"
+            }
+         ]
+      },
+      {
+         "category":"<b>Color Themes</b>",
+         "fields":[
+            {
+               "type":"color",
+               "fieldName":"theme_color",
+               "tooltip":"Text color",
+               "label":"Text color:"
+            },
+            {
+               "type":"color",
+               "fieldName":"theme_bg_color",
+               "tooltip":"Background color for panel",
+               "label":"Panel background color:"
+            }
+         ]
+      },
+      {
+         "category":"<b>Tools</b>",
+         "fields":[
+            {
+               "type":"boolean",
+               "fieldName":"home",
+               "label":"Home Extent Button"
+            }
+         ]
+      }
+   ],
+   "values":{
+      "home":true,
+      "theme_color":"#575757",
+      "theme_bg_color":"#fff"
+   }
 }
