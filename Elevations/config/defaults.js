501738e101f16e15c1500fc4d8ff00cdbd0fa80b
Kelly Hutchins <khutchins@esri.com>
2015-02-09 17:10:25 -0800

    Fix issues with rename of measure widget and add search widget.

--- Elevations/config/defaults.js
+++ Elevations/config/defaults.js
@@ -6,7 +6,18 @@ define({
     "bingmapskey": null,
     "sharinghost": location.protocol + "//" + "www.arcgis.com",
     "scalebarUnits": "dual",
-    "geocoder": false,  
+    //Set config properties for all of these 
+    "geocoder": false,  //false in config values 
+    "searchExtent": true,
+    "searchLayers":[{
+        "id": "",
+        "fields": []
+    }],
+    "locationSearch": false,  
+    "titleInHeader": true,  //true in config values 
+    "displaypopups": true, //true in config values
+    "showHelpOnLoad": true, //true in config values 
+    //end set config 
     "helperServices": {
     "geometry": {
       "url": null
