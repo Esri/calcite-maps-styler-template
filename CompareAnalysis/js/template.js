862e95febb305293f0a383fe19bd5e044296aff8
Kelly Hutchins <khutchins@esri.com>
2015-02-17 15:24:30 -0800

    Update template.js
    moved logic from main.js to temlpate.js

--- CompareAnalysis/js/template.js
+++ CompareAnalysis/js/template.js
@@ -369,6 +369,14 @@ define(["dojo/Evented", "dojo/_base/declare", "dojo/_base/kernel", "dojo/_base/a
           // use default webmap for boilerplate
           this.config.webmap = "24e01ef45d40423f95300ad2abc5038a";
         }
+        //Configured apps will have web map as an array. 
+       if(lang.isArray(this.config.webmap)){
+            this.config.webmaps = this.config.webmap;
+            this.config.webmap = this.config.webmaps[0];
+        }else{
+            this.config.webmaps.push(this.config.webmap);
+        }
+  
         arcgisUtils.getItem(this.config.webmap).then(lang.hitch(this, function (itemInfo) {
           // Set the itemInfo config option. This can be used when calling createMap instead of the webmap id
           this.displayItemConfig.itemInfo = itemInfo;
