2f6576448078d928a9d8b86b6e443ac47e866a25
Kelly Hutchins <khutchins@esri.com>
2015-05-14 17:08:59 -0700

    Update to use new search config

--- SimpleViewer/config/defaults.js
+++ SimpleViewer/config/defaults.js
@@ -17,7 +17,16 @@ define([], function() {
         //title bar. 
         "theme": "#949494",
         "find": null,
-        "feature":null,
+        //Setup the app to support a custom url parameter. Use this if you want users
+        //to be able to search for a string field in a layer. For example if the web map
+        //has parcel data and you'd like to be able to zoom to a feature using its parcel id
+        //you could add a custom url param named parcel then users could enter 
+        //a value for that param in the url. index.html?parcel=3203
+        "customUrlLayer":{
+            "id": null,//id of the search layer as defined in the web map
+            "fields": []//Name of the string field to search 
+        },
+        "customUrlParam": null,//Name of url param. For example parcels
         //Specify the color used for the closeable drawer titles.  
         "paneltheme": "#ededed",
         "bingmapskey": "", //Enter the url to your organizations bing maps key if you want to use bing basemaps
