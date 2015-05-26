2ac95b3b481797488111425ba47e06d7312f5b07
Kelly Hutchins <khutchins@esri.com>
2015-05-26 13:07:16 -0700

    Update configurationPanel.js
    update for #775

--- Viewer/resources/configurationPanel.js
+++ Viewer/resources/configurationPanel.js
@@ -200,7 +200,12 @@
                },
                "fieldOptions":{  
                   "supportedTypes":[  
-                     "esriFieldTypeString"
+                     "esriFieldTypeString",                       
+                     "esriFieldTypeDate",
+                     "esriFieldTypeSmallInteger",
+                     "esriFieldTypeInteger",
+                     "esriFieldTypeSingle",
+                     "esriFieldTypeDouble"
                   ]
                }
             },{  
