b69cbcaa0b613352885bd769d272fa39bafa51d1
Kelly Hutchins <khutchins@esri.com>
2015-09-17 10:12:34 -0700

    Ensure title text is translated for popup buttons for #897

--- Viewer/js/PopupPanel.js
+++ Viewer/js/PopupPanel.js
@@ -51,6 +51,11 @@ function(
       on(this.popup, "clear-features", lang.hitch(this, this._clearPopupSelection));
       on(this.popup, "set-features", lang.hitch(this, this._displayPopupContent));
       
+      //setup title text for popup buttons
+      console.log(esriStrings.widgets.popup);
+      dom.byId("popupButtonClose").title = esriStrings.widgets.popup.NLS_close;
+      dom.byId("popupButtonNext").title = esriStrings.widgets.popup.NLS_nextFeature;
+      dom.byId("popupButtonPrev").title = esriStrings.widgets.popup.NLS_prevFeature;
       // make nav links clickable
       on(query(".popupButton"), "click", lang.hitch(this, function(e){
         if(e.target.className === "popupButton close"){
