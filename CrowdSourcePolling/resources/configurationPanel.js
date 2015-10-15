d87ff3beb6a0852b1f2c01607265e405a1a82eeb
MikeTschudi <mtschudi@esri.com>
2015-10-15 09:23:44 -0700

    CrowdsourcePolling: updated online app template

--- CrowdSourcePolling/resources/configurationPanel.js
+++ CrowdSourcePolling/resources/configurationPanel.js
@@ -2,16 +2,9 @@
     "values": {
         "webmap": "56197689ee7e4a8aa9f0d8da09ffe721",
         "title": "Crowdsource Polling",
+        "titleIcon": "images/banner.png",
         "displayText": "<p><b>Welcome to the crowdsource polling application</b></p><p>Use Crowdsource Polling to provide information and collect feedback on plans and issues around your community.</p><p>Search for a location or click an item in the list to get started.</p>",
         "color": "#206bdb",
-        "featureLayer": {
-            "id": "LandUseCasesVotesComments_8488",
-            "fields": [{
-                "id": "itemVotesField",
-                "fields": ["VOTES"]
-            }]
-        },
-        "commentNameField": "NAME",
         "allowFacebook": false,
         "facebookAppId": "",
         "allowGoogle": false,
@@ -31,6 +24,11 @@
             "fieldName": "title",
             "type": "string",
             "tooltip": "Application name (max 23 chars)"
+        }, {
+            "label": "Icon URL",
+            "fieldName": "titleIcon",
+            "type": "string",
+            "tooltip": "Icon in top left corner of application. Icon should be 48px high."
         }, {
             "label": "Help Text",
             "fieldName": "displayText",
@@ -58,8 +56,7 @@
                 "label": "Votes Field",
                 "tooltip": "Numeric field in the selected layer for tracking the votes received for each feature."
             }]
-        }
-        ]
+        }]
     }, {
         "category": "<b>Social Media Settings</b>",
         "fields": [{
@@ -105,9 +102,7 @@
             "placeholder": "",
             "tooltip": "Text to display in the Sign In window",
             "stringFieldOption": "richtext"
-        }
-        ]
-    }
-    ]
+        }]
+    }]
 }
 
