f95d84370ad0593d47e67b38ed3e3d923b6d9986
MikeTschudi <mtschudi@esri.com>
2015-12-14 10:54:07 -0800

    Finder/FEF JSLint updates

--- Solutions/js/nls/ar/resources.js
+++ Solutions/js/nls/ar/resources.js
@@ -15,7 +15,7 @@
  | limitations under the License.
  */
 define(
-     ({
+    ({
         map: {
             error: "يتعذر إنشاء الخريطة"
         },
--- Solutions/js/nls/de/resources.js
+++ Solutions/js/nls/de/resources.js
@@ -15,7 +15,7 @@
  | limitations under the License.
  */
 define(
-     ({
+    ({
         map: {
             error: "Karte kann nicht erstellt werden"
         },
