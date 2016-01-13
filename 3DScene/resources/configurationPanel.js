fdce3f219e888fe0483a97629a360c6c31b585ea
Sajit Thomas <sthomas@esri.com>
2016-01-13 14:09:24 -0500

    Add config panel for 3DScene

---
+++ 3DScene/resources/configurationPanel.js
@@ -0,0 +1,37 @@
+{
+  "configurationSettings": [{
+    "category": "<b>App Settings</b>",
+    "fields": [{
+      "type": "webscene",
+      "label": "Select a scene"
+    }, {
+      "type": "string",
+      "fieldName": "title",
+      "label": "Title",
+      "tooltip": "Title",
+      "placeHolder": "Title"
+    }, {
+      "type": "string",
+      "fieldName": "subtitle",
+      "label": "Subtitle",
+      "tooltip": "Subtitle",
+      "placeHolder": "Subtitle"
+    }, {
+      "type": "color",
+      "fieldName": "color",
+      "label": "Color",
+      "tooltip": "Color"
+    }, {
+      "type": "boolean",
+      "fieldName": "reverse",
+      "label": "Dark theme for map",
+      "tooltip": "Dark theme for map"
+    }]
+  }],
+  "values": {
+    "title": "",
+    "subtitle": "",
+    "color": "#2cbade",
+    "reverse": true
+  }
+}
