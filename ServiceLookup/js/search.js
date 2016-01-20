d539f92d2683aacf8b5499fea3b8be0cb5c6889d
Michael Miller <mmiller@esri.com>
2016-01-20 15:58:13 -0500

    minor fix for https://devtopia.esri.com/WebGIS/arcgis-portal-app-templates/issues/1050

--- ServiceLookup/js/search.js
+++ ServiceLookup/js/search.js
@@ -84,8 +84,8 @@ function (
         var searchSources = new SearchSources(searchOptions);
         var createdOptions = searchSources.createOptions();
 
-        if (this.config.searchConfig != null && this.config.searchConfig != undefined){
-          if (this.config.searchConfig.activeSourceIndex != null && this.config.searchConfig.activeSourceIndex != undefined) {
+        if (this.config.searchConfig !== null && this.config.searchConfig !== undefined){
+          if (this.config.searchConfig.activeSourceIndex !== null && this.config.searchConfig.activeSourceIndex !== undefined) {
             createdOptions.activeSourceIndex = this.config.searchConfig.activeSourceIndex;
           }
         }
