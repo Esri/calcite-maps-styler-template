6303b52c2da27a008651ce1f6147873f98757867
Allison Muise <amuise@esri.com>
2016-01-28 09:27:10 -0800

    updated Polling configuration panel

--- CrowdSourcePolling/resources/configurationPanel.js
+++ CrowdSourcePolling/resources/configurationPanel.js
@@ -21,35 +21,41 @@
         "socialMediaDisclaimer": "Choose how you would like to sign in to this application. The name associated with your social media account will be added to any comments you post."
     },
     "configurationSettings": [{
-        "category": "<b>General Settings</b>",
+        "category": "<b>App Settings</b>",
         "fields": [{
             "label": "Select a map",
             "fieldName": "webmap",
             "type": "webmap",
             "tooltip": "Web map displayed in the application"
         }, {
-            "label": "Title",
+            "label": "Application title",
             "fieldName": "title",
             "type": "string",
             "tooltip": "Application name (max 23 chars)"
         }, {
-            "label": "Icon URL",
+            "label": "URL of application logo",
             "fieldName": "titleIcon",
             "type": "string",
             "tooltip": "Icon in top left corner of application. Icon should be 48px high."
         }, {
-            "label": "Help Text",
-            "fieldName": "displayText",
-            "type": "string",
-            "tooltip": "Text displayed in the help window. HTML tags can be used for formatting.",
-            "stringFieldOption": "richtext"
-        }, {
-            "label": "Color Scheme",
+            "label": "Color scheme",
             "fieldName": "color",
             "type": "color",
             "tooltip": "Color scheme for app"
         }, {
-            "label": "Layer",
+            "label": "Help widget text",
+            "fieldName": "displayText",
+            "type": "string",
+            "tooltip": "Text displayed in the help window. HTML tags can be used for formatting.",
+            "stringFieldOption": "richtext"
+        }]
+    }, {
+        "category": "<b>Content Settings</b>",
+        "fields": [{
+            "type": "paragraph",
+            "value": "Configure how the application will interact with your layers."
+        }, {
+            "label": "Polling feature layer",
             "fieldName": "featureLayer",
             "type": "layerAndFieldSelector",
             "tooltip": "Point, line, or polygon layer containing the features to present in the application.",
@@ -61,12 +67,12 @@
                 "supportedTypes": ["esriFieldTypeSmallInteger", "esriFieldTypeInteger"],
                 "multipleSelection": false,
                 "fieldName": "itemVotesField",
-                "label": "Votes Field",
+                "label": "Field to store the vote tally for each report (optional)",
                 "tooltip": "Numeric field in the selected layer for tracking the votes received for each feature."
             }]
         }]
     }, {
-        "category": "<b>Social Media Settings</b>",
+        "category": "<b>Access Settings</b>",
         "fields": [{
             "type": "paragraph",
             "value":"Allow your users to sign in to this application using their social media credentials. Comments submitted by authenticated users will have the commenters name automatically stored with their feedback."
@@ -74,10 +80,15 @@
             "type": "paragraph",
             "value":"See the <a href='http://solutions.arcgis.com/local-government/help/crowdsource-polling/get-started/configure-social-media-signin/' target='_blank'>help</a> for the steps to register your app with Facebook and Google+."
         },{
-            "label": "Commenter Name Field",
+            "label": "Field to store the name of authenticated commenters.",
             "fieldName": "commentNameField",
             "type": "string",
             "tooltip": "Field in a table with a geodatabase relationship to the selected feature layer for storing the name of authenticated commenters. Table must exist in map."
+        }, {
+            "label": "Allow users to sign in using Twitter",
+            "fieldName": "allowTwitter",
+            "type": "boolean",
+            "tooltip": "Enable to allow users to sign in using their Twitter credentials"
         }, {
             "label": "Allow users to sign in using Facebook",
             "fieldName": "allowFacebook",
@@ -99,12 +110,7 @@
             "type": "string",
             "tooltip": "Client ID from registering this application with Google+"
         }, {
-            "label": "Allow users to sign in using Twitter",
-            "fieldName": "allowTwitter",
-            "type": "boolean",
-            "tooltip": "Enable to allow users to sign in using their Twitter credentials"
-        }, {
-            "label": "Social Media Info",
+            "label": "Sign in window text",
             "fieldName": "socialMediaDisclaimer",
             "type": "string",
             "placeholder": "",
