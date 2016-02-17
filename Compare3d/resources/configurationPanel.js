bcc3595b6b521246869f8c870c1b0fc4fd75e6a1
Kelly Hutchins <khutchins@esri.com>
2016-02-17 15:49:46 -0800

    Compare 3d updates for #1102

--- Compare3d/resources/configurationPanel.js
+++ Compare3d/resources/configurationPanel.js
@@ -1,60 +1,62 @@
 {
-    "configurationSettings": [
-        {
-            "category": "App Settings",
-            "fields": [
-                {
-                "type": "appproxies"
-                },
-                {
-                    "type": "webscene",
-                    "selection": "multiple"
-                }
-            ]
-        },{
-            "category": "View Settings",
-            "fields":[
-              {
-                  "type":"boolean",
-                  "fieldName": "link",
-                  "label": "Link Views",
-                  "tooltip": "When true view extents linked at start"
-              },{
-                    "type":"boolean",
-                    "fieldName": "panelsOpen",
-                    "label": "Open info panels at start"
-                }
-
-            ]
-        },
-        {
-            "category": "Color Theme",
-            "fields": [
-              {
-                  "type": "color",
-                  "fieldName": "backgroundColor",
-                  "tooltip": "Background color",
-                  "label": "Background color:"
-              },
-              {
-                  "type": "color",
-                  "fieldName": "textColor",
-                  "tooltip": "Text color",
-                  "label": "Text color:"
-              },{
-                "type": "color",
-                "fieldName": "slideColorTheme",
-                "tooltip": "Slide gallery color theme",
-                "label": "Slide gallery color theme"
-              }
-            ]
-        }
-    ],
-    "values": {
-        "link": true,
-        "panelsOpen": true,
-        "color": "#fff",
-        "backgroundColor": "#333",
-        "slideColorTheme": "#05668D"
-    }
+   "configurationSettings":[
+      {
+         "category":"Scene",
+         "fields":[
+           {
+              "type":"webscene",
+              "selection":"multiple"
+           },
+            {
+               "type":"appproxies"
+            }
+         ]
+      },
+      {
+         "category":"Theme",
+         "fields":[
+            {
+               "type":"color",
+               "fieldName":"backgroundColor",
+               "tooltip":"Background color",
+               "label":"Background color:"
+            },
+            {
+               "type":"color",
+               "fieldName":"textColor",
+               "tooltip":"Text color",
+               "label":"Text color:"
+            },
+            {
+               "type":"color",
+               "fieldName":"slideColorTheme",
+               "tooltip":"Slide gallery color theme",
+               "label":"Slide gallery color theme"
+            }
+         ]
+      },
+      {
+         "category":"Options",
+         "fields":[
+            {
+               "type":"boolean",
+               "fieldName":"link",
+               "label":"Link Views",
+               "tooltip":"When true view extents linked at start"
+            },
+            {
+               "type":"boolean",
+               "fieldName":"panelsOpen",
+               "label":"Open info panels at start"
+            }
+         ]
+      }
+   ],
+   "values":{
+      "link":true,
+      "panelsOpen":true,
+      "color":"#fff",
+      "backgroundColor":"#333",
+      "slideColorTheme":"#05668D"
+   }
 }
