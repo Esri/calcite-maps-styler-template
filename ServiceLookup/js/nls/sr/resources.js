781ec91c1b2fcbcc2308ee5bc419a58c7d788323
Michael Miller <mmiller@esri.com>
2016-03-31 13:31:41 -0400

    Updated Info Lookup for https://devtopia.esri.com/WebGIS/arcgis-portal-app-templates/issues/1152

---
+++ ServiceLookup/js/nls/sr/resources.js
@@ -0,0 +1,53 @@
+define({
+    root: ({
+        map: {
+            error: "Unable to create map",
+            mouseToolTip: "Click on the map to check for service"
+        },
+        error: {
+            layerNotFound: "Layer is not in the webmap",
+            fieldNotFound: "Field not found",
+            popupNotSet: "Popup not enabled for this layer",
+            noLayersSet: "No layers defined in the config, the search box will not work"
+        },
+        page: {
+            title: "Information Lookup",
+        },
+        ui:{
+            basemapButton: "Basemap"
+        },
+        popup: {
+          urlMoreInfo: "More Info",
+          attachments: "Attachments"
+        }
+
+
+    }),
+    "ar": 1,
+    "cs": 1,
+    "da": 1,
+    "de": 1, 
+    "el": 1,
+    "es": 1,
+    "et": 1,
+    "fi": 1,
+    "fr": 1,
+    "he": 1,
+    "it": 1,
+    "ja": 1,
+    "ko": 1,
+    "lt": 1,
+    "lv": 1,
+    "nl": 1,
+    "nb": 1,
+    "pl": 1,
+    "pt-br": 1,
+    "pt-pt": 1,
+    "ro": 1,
+    "ru": 1,
+    "sv": 1,
+    "th": 1,
+    "tr": 1,
+    "zh-cn": 1,
+    "vi": 1
+});
