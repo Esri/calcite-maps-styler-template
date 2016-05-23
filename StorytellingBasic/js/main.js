48e73c135d012ba80c2d903e7a570234e1a2aacc
Kelly Hutchins <khutchins@esri.com>
2016-05-23 15:51:25 -0700

    Style tweaks to match 3.17 updates

--- StorytellingBasic/js/main.js
+++ StorytellingBasic/js/main.js
@@ -42,21 +42,21 @@ define([
   "application/MapUrlParams",
 
   "dojo/domReady!"
-], function (
+], function(
   declare, lang,
   Deferred,
   query, on,
   Toggler, coreFx,
   dom, domAttr, domClass, domConstruct, domStyle,
   registry,
-  arcgisUtils, 
-  domUtils, 
+  arcgisUtils,
+  domUtils,
   HomeButton,
   MapUrlParams
 ) {
   return declare(null, {
     config: {},
-    startup: function (config) {
+    startup: function(config) {
       var promise;
       // config will contain application and user defined info for the template such as i18n strings, the web map id
       // and application id
@@ -65,15 +65,15 @@ define([
         this.config = config;
         // Hide legend container if not enabled
         dom.byId("legTogText").innerHTML = this.config.i18n.legendToggle.label;
-        if(!this.config.legend){
+        if (!this.config.legend) {
           domUtils.hide(dom.byId("legendCon"));
         }
-        // Hide header if embed is specified 
-        if(this.config.embed || this.config.headerHeight == "0"){
+        // Hide header if embed is specified
+        if (this.config.embed || this.config.headerHeight == "0") {
           domUtils.hide(dom.byId("header"));
           registry.byId("mainWindow").layout();
-        }else{
-          // Set header height 
+        } else {
+          // Set header height
           domStyle.set(dom.byId("header"), "height", this.config.headerHeight + "px");
           registry.byId("mainWindow").layout();
         }
@@ -81,19 +81,19 @@ define([
         //supply either the webmap id or, if available, the item info
         var itemInfo = this.config.itemInfo || this.config.webmap;
         var mapParams = new MapUrlParams({
-               center: this.config.center || null,
-               extent: this.config.extent || null,
-               level: this.config.level || null,
-               marker: this.config.marker || null,
-               mapSpatialReference: itemInfo.itemData.spatialReference,
-               defaultMarkerSymbol: this.config.markerSymbol,
-               defaultMarkerSymbolWidth: this.config.markerSymbolWidth,
-               defaultMarkerSymbolHeight: this.config.markerSymbolHeight,
-               geometryService: this.config.helperServices.geometry.url
-        });     
-        mapParams.processUrlParams().then(lang.hitch(this, function(urlParams){
+          center: this.config.center || null,
+          extent: this.config.extent || null,
+          level: this.config.level || null,
+          marker: this.config.marker || null,
+          mapSpatialReference: itemInfo.itemData.spatialReference,
+          defaultMarkerSymbol: this.config.markerSymbol,
+          defaultMarkerSymbolWidth: this.config.markerSymbolWidth,
+          defaultMarkerSymbolHeight: this.config.markerSymbolHeight,
+          geometryService: this.config.helperServices.geometry.url
+        });
+        mapParams.processUrlParams().then(lang.hitch(this, function(urlParams) {
           this._createWebMap(itemInfo, urlParams);
-        }), lang.hitch(this, function(error){
+        }), lang.hitch(this, function(error) {
           this.reportError(error);
         }));
       } else {
@@ -105,7 +105,7 @@ define([
       }
       return promise;
     },
-    reportError: function (error) {
+    reportError: function(error) {
       // remove loading class from body
       domClass.remove(document.body, "app-loading");
       domClass.add(document.body, "app-error");
@@ -126,7 +126,7 @@ define([
     },
 
     // create a map based on the input web map id
-    _createWebMap: function (itemInfo,params) {
+    _createWebMap: function(itemInfo, params) {
 
       // create webmap from item
       return arcgisUtils.createMap(itemInfo, "mapDiv", {
@@ -135,116 +135,116 @@ define([
         layerMixins: this.config.layerMixins || [],
         editable: this.config.editable,
         bingMapsKey: this.config.bingKey
-      }).then(lang.hitch(this, function (response) {
+      }).then(lang.hitch(this, function(response) {
 
         this.map = response.map;
-        if(params.markerGraphic){
-            // Add a marker graphic with an optional info window if
-            // one was specified via the marker url parameter
-            require(["esri/layers/GraphicsLayer"], lang.hitch(this, function(GraphicsLayer){
-              var markerLayer = new GraphicsLayer();
+        if (params.markerGraphic) {
+          // Add a marker graphic with an optional info window if
+          // one was specified via the marker url parameter
+          require(["esri/layers/GraphicsLayer"], lang.hitch(this, function(GraphicsLayer) {
+            var markerLayer = new GraphicsLayer();
 
-              this.map.addLayer(markerLayer);
-              markerLayer.add(params.markerGraphic);
+            this.map.addLayer(markerLayer);
+            markerLayer.add(params.markerGraphic);
 
-              if(params.markerGraphic.infoTemplate){
-                this.map.infoWindow.setFeatures([params.markerGraphic]);
-                this.map.infoWindow.show(params.markerGraphic.geometry);
-              }
-            }));
+            if (params.markerGraphic.infoTemplate) {
+              this.map.infoWindow.setFeatures([params.markerGraphic]);
+              this.map.infoWindow.show(params.markerGraphic.geometry);
+            }
+          }));
 
         }
         // remove loading class from body
         domClass.remove(document.body, "app-loading");
         this._updateTheme();
 
-        // Set default title and sub title 
+        // Set default title and sub title
         this.config.title = this.config.title || response.itemInfo.item.title || "";
         this.config.subtitle = this.config.subtitle || response.itemInfo.item.snippet || "";
 
         document.title = this.config.title;
-        if(this.config.showTitle){
-         dom.byId("title").innerHTML = this.config.title;
+        if (this.config.showTitle) {
+          dom.byId("title").innerHTML = this.config.title;
         }
-        if(this.config.showSubTitle){
+        if (this.config.showSubTitle) {
           dom.byId("subtitle").innerHTML = this.config.subtitle;
         }
-        // Add the logo 
-        if(this.config.showLogo && this.config.logo ){
-          if(this.config.logoLink){
+        // Add the logo
+        if (this.config.showLogo && this.config.logo) {
+          if (this.config.logoLink) {
             dom.byId("logoLink").href = this.config.logoLink;
           }
           dom.byId("logoImg").src = this.config.logo;
         }
         // Add the social media text to the header
-        if(this.config.showSocialText && this.config.socialText){
-          domConstruct.create("a",{
+        if (this.config.showSocialText && this.config.socialText) {
+          domConstruct.create("a", {
             href: this.config.socialLink || "#",
-            target : "_blank",
+            target: "_blank",
             innerHTML: this.config.socialText
-          },"linkContainer", "first");       
+          }, "linkContainer", "first");
         }
         // Show social icons
-        if(this.config.showSocialIcons){
-          domAttr.set("facebook","title", this.config.i18n.social.facebook);
-          domAttr.set("twitter","title", this.config.i18n.social.twitter);
-          domAttr.set("link","title", this.config.i18n.social.link);
+        if (this.config.showSocialIcons) {
+          domAttr.set("facebook", "title", this.config.i18n.social.facebook);
+          domAttr.set("twitter", "title", this.config.i18n.social.twitter);
+          domAttr.set("link", "title", this.config.i18n.social.link);
 
           query(".shareIcon").style("display", "inline-block");
           // Setup click events for sharing nodes
-          require(["application/Share"],lang.hitch(this, function(Share){
+          require(["application/Share"], lang.hitch(this, function(Share) {
             var share = new Share({
               config: this.config,
               map: this.map,
               title: this.config.title,
               summary: this.config.subtitle
             });
-            query(".shareIcon").on("click", lang.hitch(this, function(node){
+            query(".shareIcon").on("click", lang.hitch(this, function(node) {
               share.shareLink(node);
             }));
           }));
         }
 
-        // Add scalebar 
-        if(this.config.scalebar){
-          require(["esri/dijit/Scalebar"], lang.hitch(this, function(Scalebar){
+        // Add scalebar
+        if (this.config.scalebar) {
+          require(["esri/dijit/Scalebar"], lang.hitch(this, function(Scalebar) {
             var scalebar = new Scalebar({
               map: this.map,
               scalebarUnit: this.config.units
             });
           }));
         }
-        // Add Home Button to zoom slider 
+        // Add Home Button to zoom slider
         var homeButton = new HomeButton({
           map: this.map
-        },domConstruct.create("div",{},query(".esriSimpleSliderIncrementButton")[0],"after"));
+        }, domConstruct.create("div", {}, query(".esriSimpleSliderIncrementButton")[0], "after"));
         homeButton.startup();
 
-        // Add legend 
-        if(this.config.legend){
+        // Add legend
+        if (this.config.legend) {
           domUtils.hide(dom.byId("legendDiv"));
-          require(["esri/dijit/Legend"], lang.hitch(this, function(Legend){
+          require(["esri/dijit/Legend"], lang.hitch(this, function(Legend) {
             var layerInfo = arcgisUtils.getLegendLayers(response);
-            if(layerInfo.length === 0){
+            if (layerInfo.length === 0) {
               // hide the legend
               domUtils.hide(dom.byId("legendCon"));
-            }else{
-     
-              // Toggle legend display 
+            } else {
+
+              // Toggle legend display
               var toggler = new Toggler({
-                node:"legendDiv",
+                node: "legendDiv",
                 showFunc: coreFx.wipeIn,
                 hideFunc: coreFx.wipeOut
               });
 
-              on(dom.byId("legendToggle"), "click", lang.hitch(this, function(){
-                var displayMode = domStyle.get(dom.byId("legendDiv"),"display");
-                if(displayMode === "none"){
-                  domClass.remove("legToggleIcon","icon-down");
+              on(dom.byId("legendToggle"), "click", lang.hitch(this, function() {
+                var displayMode = domStyle.get(dom.byId("legendDiv"), "display");
+                if (displayMode === "none") {
+                  domClass.remove("legToggleIcon", "icon-down");
                   domClass.add("legToggleIcon", "icon-up");
                   toggler.show();
-                }else{
-                  domClass.add("legToggleIcon","icon-down");
+                } else {
+                  domClass.add("legToggleIcon", "icon-down");
                   domClass.remove("legToggleIcon", "icon-up");
                   toggler.hide();
                 }
@@ -253,59 +253,59 @@ define([
               var legend = new Legend({
                 map: this.map,
                 layerInfos: layerInfo
-              },"legendDiv");
+              }, "legendDiv");
               legend.startup();
             }
           }));
 
         }
 
-        // Add search 
-        if(this.config.search){
-          require(["esri/dijit/Search", "esri/tasks/locator", "application/SearchSources"], lang.hitch(this, function(Search, Locator, SearchSources){
-             var searchOptions = {
-                  map: this.map,
-                  useMapExtent: this.config.searchExtent,
-                  itemData: response.itemInfo.itemData
-             };
-             if(this.config.searchConfig){  
+        // Add search
+        if (this.config.search) {
+          require(["esri/dijit/Search", "esri/tasks/locator", "application/SearchSources"], lang.hitch(this, function(Search, Locator, SearchSources) {
+            var searchOptions = {
+              map: this.map,
+              useMapExtent: this.config.searchExtent,
+              itemData: response.itemInfo.itemData
+            };
+            if (this.config.searchConfig) {
               searchOptions.applicationConfiguredSources = this.config.searchConfig.sources || [];
-             }else if(this.config.searchLayers){
+            } else if (this.config.searchLayers) {
               var configuredSearchLayers = (this.config.searchLayers instanceof Array) ? this.config.searchLayers : JSON.parse(this.config.searchLayers);
               searchOptions.configuredSearchLayers = configuredSearchLayers;
               searchOptions.geocoders = this.config.locationSearch ? this.config.helperServices.geocode : [];
-             }
-              var searchSources = new SearchSources(searchOptions);
-              var createdOptions = searchSources.createOptions();
-          
-              if(this.config.searchConfig && this.config.searchConfig.activeSourceIndex){
-                  createdOptions.activeSourceIndex = this.config.searchConfig.activeSourceIndex;
-              }
-              createdOptions.enableButtonMode = true;
-              var search = new Search(createdOptions, domConstruct.create("div"));
+            }
+            var searchSources = new SearchSources(searchOptions);
+            var createdOptions = searchSources.createOptions();
 
-              search.startup();
+            if (this.config.searchConfig && this.config.searchConfig.activeSourceIndex) {
+              createdOptions.activeSourceIndex = this.config.searchConfig.activeSourceIndex;
+            }
+            createdOptions.enableButtonMode = true;
+            var search = new Search(createdOptions, domConstruct.create("div"));
 
-              if (search && search.domNode) {
-                  domConstruct.place(search.domNode, "search");
-              }
+            search.startup();
+
+            if (search && search.domNode) {
+              domConstruct.place(search.domNode, "search");
+            }
 
           }));
         }
 
         // return for promise
         return response;
-        // map has been created. You can start using it.
-        // If you need map to be loaded, listen for it's load event.
+      // map has been created. You can start using it.
+      // If you need map to be loaded, listen for it's load event.
       }), this.reportError);
     },
-    _updateTheme: function(){
-      // update app theme 
+    _updateTheme: function() {
+      // update app theme
       query(".fg").style("color", this.config.color.toString());
       // Alt color (subtitle, social logo)
       query(".ac").style("color", this.config.subtitleColor.toString());
       query(".bg").style("backgroundColor", this.config.background.toString());
-      query(".esriPopup .pointer").style("backgroundColor", this.config.background.toString());
+      //query(".esriPopup .pointer").style("backgroundColor", this.config.background.toString());
       query(".esriPopup .titlePane").style({
         "backgroundColor": this.config.background.toString(),
         "color": this.config.color.toString()
