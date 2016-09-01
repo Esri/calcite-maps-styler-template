bda710e12218cbbf4d29f22b63d2c826294555ca
Kelly Hutchins <khutchins@esri.com>
2016-09-01 10:40:39 -0700

    Update to display editor when it is active tool for #1299

--- Viewer_New/js/main.js
+++ Viewer_New/js/main.js
@@ -294,7 +294,6 @@ define([
           }
 
           // if no tools are active let's setup support for setting
-          //this._setActiveTool(toolbar);
           on(toolbar, "updateTool", lang.hitch(this,
             function(name) {
               if (name === "measure") {
@@ -324,7 +323,6 @@ define([
                   }));
               }
             }));
-
           domStyle.set("panelPages", "visibility",
             "visible");
           //Now that all the tools have been added to the toolbar we can add page naviagation
@@ -352,7 +350,6 @@ define([
     _showSplashScreen: function(toolbar) {
       // Setup the modal overlay if enabled
       if (this.config.splashModal) {
-
         domClass.add(document.body, "noscroll");
         domClass.remove("modal", "hide");
         domAttr.set("modal", "aria-label", this.config.splashTitle || "Splash Screen");
@@ -399,7 +396,6 @@ define([
       } else {
         deferred.resolve(false);
       }
-
       return deferred.promise;
     },
 
@@ -465,7 +461,6 @@ define([
 
     },
     _addEditor: function(tool, toolbar, panelClass) {
-
       //Add the editor widget to the toolbar if the web map contains editable layers
       var deferred = new Deferred();
       this.editableLayers = this._getEditableLayers(this.config.response
@@ -481,7 +476,6 @@ define([
       } else {
         deferred.resolve(false);
       }
-
       return deferred.promise;
     },
     _createEditor: function() {
@@ -494,7 +488,6 @@ define([
           deferred.resolve(false);
           return;
         }
-
         //add field infos if necessary. Field infos will contain hints if defined in the popup and hide fields where visible is set
         //to false. The popup logic takes care of this for the info window but not the edit window.
         array.forEach(this.editableLayers, lang.hitch(this,
@@ -508,7 +501,6 @@ define([
               var fieldInfos = [];
               array.forEach(fields, lang.hitch(this,
                 function(field) {
-
                   //added support for editing date and time
                   if (field.format && field.format.dateFormat &&
                     array.indexOf(this.timeFormats,
@@ -532,29 +524,32 @@ define([
           toolbarVisible: has("edit-toolbar")
         };
         this.map.enableSnapping();
+        //show the editor panel so layers render in the widget correctly.
+        domClass.remove(dom.byId("page_edit"), "hide");
         this.editor = new Editor({
           settings: settings
         }, domConstruct.create("div", {}, this.editorDiv));
-
-
+        this.editor.on("load", lang.hitch(this, function() {
+          // hide the panel
+          domClass.add(dom.byId("page_edit"), "hide");
+          deferred.resolve(true);
+        }), function(error) {
+          domClass.add(dom.byId("page_edit"), "hide");
+          deferred.resolve(true);
+        });
         this.editor.startup();
-        deferred.resolve(true);
-
       }));
       return deferred.promise;
-
     },
     _destroyEditor: function() {
       if (this.editor) {
         this.editor.destroy();
         this.editor = null;
       }
-
     },
     _addLayers: function(tool, toolbar, panelClass) {
       //Toggle layer visibility if web map has operational layers
       var deferred = new Deferred();
-
       var layers = this.config.response.itemInfo.itemData.operationalLayers;
 
       if (layers.length === 0) {
@@ -622,7 +617,6 @@ define([
       }
       return deferred.promise;
     },
-
     _addMeasure: function(tool, toolbar, panelClass) {
       //Add the measure widget to the toolbar.
       var deferred = new Deferred();
@@ -681,6 +675,7 @@ define([
               height: "auto"
             }, domConstruct.create("div", {}, ovMapDiv));
             ovMap.startup();
+
             query(".ovwHighlight").forEach(function(node) {
               domAttr.set(node, "tabindex", "0");
               domAttr.set(node, "aria-label", this.config.i18n.map.overviewDetails);
@@ -991,7 +986,7 @@ define([
               .activeSourceIndex;
           }
           createdOptions.enableSearchingAll = false;
-          if (this.config.searchConfig && this.config.searchConfig.enableSearchingAll && this.config.searchConfig.enableSearchingAll == true) {
+          if (this.config.searchConfig && this.config.searchConfig.enableSearchingAll && this.config.searchConfig.enableSearchingAll === true) {
             createdOptions.enableSearchingAll = true;
           }
         }
