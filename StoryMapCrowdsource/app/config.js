999e81de40683e93bc90b51b69b798271bbfb35f
Gregory L'Azou <glazou@esri.com>
2016-09-07 10:31:57 -0700

    crowdsource 09/07

--- StoryMapCrowdsource/app/config.js
+++ StoryMapCrowdsource/app/config.js
@@ -1,4 +1,4 @@
-/*! story-map-crowdsource - v0.2.0 - 2016-09-06, 03:49:11 PM - This application is released under the Apache License V2.0 by Esri http://www.esri.com/ - https://github.com/Esri/crowdsource-storytelling-template-js */'use strict';
+/*! story-map-crowdsource - v0.2.0 - 2016-09-07, 01:25:28 PM - This application is released under the Apache License V2.0 by Esri http://www.esri.com/ - https://github.com/Esri/crowdsource-storytelling-template-js */'use strict';
 
 window.app.cfg = {
 	BITLY_API_KEY: {
