61d6ee43bc5e6d44a793b30eb4e14cbe09e0d72e
Stephen Sylvia <ssylvia@esri.com>
2016-09-12 14:40:31 -0400

    update sm-crowdsource

--- StoryMapCrowdsource/app/config.js
+++ StoryMapCrowdsource/app/config.js
@@ -1,4 +1,4 @@
-/*! story-map-crowdsource - v0.2.0 - 2016-09-09, 11:42:47 AM - This application is released under the Apache License V2.0 by Esri http://www.esri.com/ - https://github.com/Esri/crowdsource-storytelling-template-js */'use strict';
+/*! story-map-crowdsource - v0.2.0 - 2016-09-12, 02:38:26 PM - This application is released under the Apache License V2.0 by Esri http://www.esri.com/ - https://github.com/Esri/crowdsource-storytelling-template-js */'use strict';
 
 window.app.cfg = {
 	BITLY_API_KEY: {
