86d9dc632428f22c4882ab762ff86e2c1c7c4b53
Allan Laframboise <alaframboise@esri.com>
2016-11-03 08:23:14 -0700

    Added resource language files

---
+++ js/application/nls/zh-cn/resources.js
@@ -0,0 +1,32 @@
+  /*global define */
+  /*
+  | Copyright 2014 Esri
+  |
+  | Licensed under the Apache License, Version 2.0 (the "License");
+  | you may not use this file except in compliance with the License.
+  | You may obtain a copy of the License at
+  |
+  |    http://www.apache.org/licenses/LICENSE-2.0
+  |
+  | Unless required by applicable law or agreed to in writing, software
+  | distributed under the License is distributed on an "AS IS" BASIS,
+  | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  | See the License for the specific language governing permissions and
+  | limitations under the License.
+  */
+define({
+  "error": "错误",
+  "menu": {
+    "title": "主菜单",
+    "items": {
+      "about": "关于",
+      "basemaps": "底图",
+      "legend": "图例",
+      "bookmarks": "试_Slide Show____验",
+      "toggleNav": "完整地图"
+    }
+  },
+  "basemaps": {
+    "select": "选择底图"
+  }
+});
---
+++ js/application/nls/zh-tw/resources.js
@@ -0,0 +1,32 @@
+  /*global define */
+  /*
+  | Copyright 2014 Esri
+  |
+  | Licensed under the Apache License, Version 2.0 (the "License");
+  | you may not use this file except in compliance with the License.
+  | You may obtain a copy of the License at
+  |
+  |    http://www.apache.org/licenses/LICENSE-2.0
+  |
+  | Unless required by applicable law or agreed to in writing, software
+  | distributed under the License is distributed on an "AS IS" BASIS,
+  | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  | See the License for the specific language governing permissions and
+  | limitations under the License.
+  */
+define({
+  "error": "錯誤",
+  "menu": {
+    "title": "主功能表",
+    "items": {
+      "about": "關於",
+      "basemaps": "底圖",
+      "legend": "圖例",
+      "bookmarks": "試_Slide Show____驗",
+      "toggleNav": "完整地圖"
+    }
+  },
+  "basemaps": {
+    "select": "選擇底圖"
+  }
+});
