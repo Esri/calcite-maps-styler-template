640ef121639f835bcfac0b35d5cbf0cbf27c51d2
Allan Laframboise <alaframboise@esri.com>
2017-02-02 06:03:34 -0800

    Language support

---
+++ js/application/nls/hi/resources.js
@@ -0,0 +1,35 @@
+  /*global define */
+  /*
+  | Copyright 2014 Esri
+  |
+  | Licensed under the Apache License, Version 2.0 (the "License");
+  | you may not use this file except in compliance with the License.
+  | You may obtain a copy of the License at
+  |
+  |    http://www.apache.org/licenses/LICENSE-2.0
+  |
+  | Unless required by applicable law or agreed to in writing, software
+  | distributed under the License is distributed on an "AS IS" BASIS,
+  | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  | See the License for the specific language governing permissions and
+  | limitations under the License.
+  */
+define({
+  "error": "त्रुटि",
+  "menu": {
+    "title": "मुख्य मेनू",
+    "items": {
+      "about": "बारे में",
+      "basemaps": "बेसमैप",
+      "legend": "लीजेंड",
+      "layers": "आ_Layers___ज",
+      "slides": "स्लाइड्स",
+      "bookmarks": "बुकमार्क्स",
+      "share": "साझा करें",
+      "toggleNav": "पूर्ण मानचित्र"
+    }
+  },
+  "basemaps": {
+    "select": "कोई बेसमैप चुनें"
+  }
+});
